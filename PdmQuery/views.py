b25a0bf887b383843a0feab6c8b408db6a6f203c
huangleixing <1090905255@qq.com>
2021-03-09 21:34:46 +0800

    电阻数据显示

--- PdmQuery/views.py
+++ PdmQuery/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from .models import ICohm
 
 
 def Search_home(request):
-    return render(request, 'PdmSearchPage/SearchHompage.html')
+    ohm_data = ICohm.objects.all()
+    return render(request, 'PdmSearchPage/SearchHompage.html', {'ohm_data': ohm_data})
