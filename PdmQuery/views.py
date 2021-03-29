7c990547275fb0a6c8ef2a02792144e957885883
huangleixing <1090905255@qq.com>
2021-03-29 21:36:58 +0800

    前端文件修改，后端post修改

--- PdmQuery/views.py
+++ PdmQuery/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from django.views.decorators.csrf import csrf_exempt,csrf_protect
 from .models import ICohm
 
-
+@csrf_protect
 def SearchHome(request):
     ohm_data = ICohm.objects.filter(OhmValue__range=[200, 1100]).filter(OhmValue__range=[500, 1100])
     return render(request, 'PdmSearchPage/SearchHompage.html', {'ohm_data': ohm_data})
