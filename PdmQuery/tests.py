d3e808aeb1f27ea0f2e0938a4c09c6d5466aa3cd
huangleixing <1090905255@qq.com>
2021-03-30 16:31:12 +0800

    前端web修改, views查询修改

--- PdmQuery/tests.py
+++ PdmQuery/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+
+b = {'test': 'dsdet', 'bds':'setqt'}
+for i, k in b.items():
+    print(b[i])
+
+
--- PdmQuery/views.py
+++ PdmQuery/views.py
@@ -3,11 +3,31 @@ from django.shortcuts import HttpResponse
 from django.views.decorators.csrf import csrf_exempt,csrf_protect
 from .models import ICohm
 
-@csrf_protect
+
+def SearchDataProcess(**kwargs):
+    for key, value in kwargs.items():
+        if kwargs[key] is None:
+            del kwargs[key]
+    return kwargs
+
+
 def SearchHome(request):
-    data = request.POST.get('')
-    ohm_data = ICohm.objects.filter(OhmValue__range=[200, 1100]).filter(OhmValue__range=[500, 1100])
-    return render(request, 'PdmSearchPage/SearchHompage.html', {'ohm_data': ohm_data, 'data': data})
+    search_type = request.POST.get('search_type')
+    search_context = request.POST.get('search_context')
+    search_kind = request.POST.get('search_context')
+    voltage_min = request.POST.get('voltage_min')
+    voltage_max = request.POST.get('voltage_max')
+    height_min = request.POST.get('height_min')
+    height_max = request.POST.get('height_max')
+    producter = request.POST.get('producter')
+    value_min = request.POST.get('value_min')
+    value_max = request.POST.get('value_max')
+    tol_min = request.POST.get('tol_min')
+    tol_max = request.POST.get('tol_max')
+    pack_type = request.POST.get('pack_type')
+
+    ohm_data = ICohm.objects.filter(OhmValue__range=[200, 1100], PartNumber__gt=10010007)
+    return render(request, 'PdmSearchPage/SearchHompage.html', {'ohm_data': ohm_data})
 
 
 def ShowDetail(request):
